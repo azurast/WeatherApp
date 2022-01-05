a67b3673de70bb67e55a2cd35fc8bc1533e2f8ec
Azura <azstima@gmail.com>
2022-01-05 15:20:46 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Azura Sakan Taufik on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
